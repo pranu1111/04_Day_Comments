a739eb43a4fdf242c74bdd72347cdab72dff3cee
pranu1111 <107885176+pranu1111@users.noreply.github.com>
2022-06-23 01:39:21 +0530

    Add files via upload

---
+++ comments_in_py.py
@@ -0,0 +1,7 @@
+print('# This is single line comment in python')    # This is single line comment in python
+
+print('\""" This is multiline comment in python \""""')   
+print("\'''This is also multiline comment in python \'''") 
+ 
+'''This is also multiline comment in python'''
+"""This is also multiline comment in python """
